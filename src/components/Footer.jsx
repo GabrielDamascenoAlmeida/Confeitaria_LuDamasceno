97f36ba0a9f2103dd83275f90ffccfcbc34b1f0f
GabrielDamascenoAlmeida <gabrieldamascenoalmeida341@gmail.com>
2025-10-26 01:51:41 -0300

    Responsividade do footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,24 +17,27 @@ export default function Footer() {
                     </div>
                     <p>Criando momentos doces e especiais com ingredientes selecionados e muito amor em cada receita.</p>
                 </div>
-                <div className={style.larguradiv} id={style.larguradiv}>
-                    <h2>Contato</h2>
-                    <div id={style.texto_meio}>
-                        <p><img src={location} alt="Ícone de " className={style.minicon}/> Rua das Delícias, 123 - Centro</p>
-                        <p><img src={phone} alt="Ícone de " className={style.minicon}/> (11) 98615-2515</p>
-                        <p><img src={letter} alt="Ícone de " className={style.minicon}/> lucinalvaalmeida@gmail.com</p>
+                <div className={style.fix}>
+                    <div className={style.larguradiv} id={style.larguradiv}>
+                        <h2>Contato</h2>
+                        <div id={style.texto_meio}>
+                            <p><img src={location} alt="Ícone de " className={style.minicon}/> Rua das Delícias, 123 - Centro</p>
+                            <p><img src={phone} alt="Ícone de " className={style.minicon}/> (11) 98615-2515</p>
+                            <p><img src={letter} alt="Ícone de " className={style.minicon}/> lucinalvaalmeida@gmail.com</p>
+                        </div>
+                    </div>
+                        <div className={style.larguradiv}>
+                        <h2>Nos siga</h2>
+                        <div id={style.sociais}>
+                            <div className={style.rede}><img src={insta} alt="Logo instagram" /></div>
+                            <div className={style.rede}><img src={face} alt="Logo facebook" /></div>
+                        </div>
+                        <p>Horário de funcionamento:</p>
+                        <p>Seg - Sex: 8h às 18h</p>
+                        <p>Sáb - Dom: 8h às 16h</p>
                     </div>
                 </div>
-                <div className={style.larguradiv}>
-                    <h2>Nos siga</h2>
-                    <div id={style.sociais}>
-                        <div className={style.rede}><img src={insta} alt="Logo instagram" /></div>
-                        <div className={style.rede}><img src={face} alt="Logo facebook" /></div>
-                    </div>
-                    <p>Horário de funcionamento:</p>
-                    <p>Seg - Sex: 8h às 18h</p>
-                    <p>Sáb - Dom: 8h às 16h</p>
-                </div>
+                
             </section>
             <br />
 
